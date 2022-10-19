6517bbac56046fbe44c3f6b4f06ef82e2655096a
Serenajandra <serena.jandra@gmail.com>
2022-10-19 15:35:09 +0200

    funzione per random numbers

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+
+const fiveRndNumbers = arrayRndNum(1, 5);
+console.log(fiveRndNumbers);
+
+function arrayRndNum(min, max) {
+    let randomNumbers = [];
+    while(randomNumbers.length < max ) {
+        for (let i = min; i < max; i++) {
+           const rndNum = Math.floor(Math.random() * (100 - 1 + 1))+ 1;  
+            if(!randomNumbers.includes(rndNum)){
+                randomNumbers.push(rndNum);
+            }
+            i++;
+        }   
+    }
+}
+
+
+// function randomNumber(1, 100) {
+//     return Math.floor(Math.random() * (100 - 1 + 1))+ min;    
+// }
+// console.log(randomNumber())
+
+
+// Dopo 30 secondi i numeri spariscono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+            // const num = arrayRndNum[i];
+            // console.log(randomNumber)
